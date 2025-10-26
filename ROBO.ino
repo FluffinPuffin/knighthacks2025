82d4620febe4c804b113cdc2d59d4f32e7715555
dphamfirst-dev <d.phamfirst@gmail.com>
2025-10-26 00:04:58 -0400

    Modify motor control logic and add Serial feedback
    New turning

--- ROBO.ino
+++ ROBO.ino
@@ -8,6 +8,7 @@ void setup() {
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   s1.attach(11);
+  s1.write(180);
 }
 
 void loop() 
@@ -15,42 +16,46 @@ void loop()
   while (Serial.available() == 0)
   {}
   int iput = Serial.parseInt();
-  Serial.println(iput);
   switch (iput)
   {
     case 1:
+      s1.write(180);
       motorSpeed = 120;
-      Serial.println(motorSpeed);
       analogWrite(5, motorSpeed);
       analogWrite(6, motorSpeed);
+      Serial.println("Done1");
       break;
     case 2:
+      s1.write(180);
       motorSpeed = 180;
       analogWrite(5, motorSpeed);
       analogWrite(6, motorSpeed);
+      Serial.println("Done2");
       break;
     case 3:
+      s1.write(180);
       motorSpeed = 240;
       analogWrite(5, motorSpeed);
       analogWrite(6, motorSpeed);
+      Serial.println("Done3");
       break;
     case 4://right
-      analogWrite(5, 150);
-      analogWrite(6, 50);
+      analogWrite(5, 120);
+      analogWrite(6, 0);
+      s1.write(135);
+      Serial.println("Done4");
       break;
     case 5://left
-      analogWrite(6, 150);
-      analogWrite(5, 50);
-      break;
-    case 6://up
-      s1.write(0);
-      break;
-    case 7://down
-      s1.write(45);
+      analogWrite(6, 120);
+      analogWrite(5, 0);
+      s1.write(135);
+      Serial.println("Done5");
       break;
     case 9:
+      s1.write(180);
       analogWrite(5, 0);
       analogWrite(6, 0);
+      Serial.println("Done9");
       break;
   }//end switch
 }
