0424d98d0eef74d90324e590b932e0b0fa01ac95
dphamfirst-dev <d.phamfirst@gmail.com>
2025-10-26 06:31:21 -0400

    Accelerometer added

--- ROBO.ino
+++ ROBO.ino
@@ -6,6 +6,7 @@
 int motorSpeed;
 int iput;
 float zoffset;
+float xoffset, yoffset;
 unsigned long lastTime = 0; 
 Servo s1;
 Adafruit_MPU6050 mpu;
@@ -23,30 +24,33 @@ void setup() {
     }
   }
   Serial.println("MPU6050 Found!");
+  mpu.setAccelerometerRange(MPU6050_RANGE_4_G);
   mpu.setGyroRange(MPU6050_RANGE_500_DEG);
   mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  delay(300);
   lastTime = micros();
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
   zoffset = g.gyro.z;
+  xoffset = a.acceleration.x;
+  yoffset = a.acceleration.y;
 }
 
 void loop() 
 {
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
-  float angleZ = 0.0f;     
-  lastTime = 0; 
-
-  // Print the turn rate (angular velocity) data
-  Serial.print("Turn Rate (rad/s) X: ");
-  Serial.print(g.gyro.x);
-  Serial.print(", Y: ");
-  Serial.print(g.gyro.y);
-  Serial.print(", Z: ");
+  float angleZ = 0.0f;
+  float position = 0.0f;
+  int counter = 0;
+  
+  Serial.print("Acceleration: " );
+  Serial.println(a.acceleration.x - xoffset);
+  Serial.println(a.acceleration.y - yoffset);
+  Serial.print("Turn rate: ");
   Serial.println(g.gyro.z - zoffset);
 
-  delay(500);
+  delay(200);
   clearInputBuffer();
   while (Serial.available() == 0)
   {}
@@ -55,28 +59,92 @@ void loop()
   if (iput == 1)
   {
     clearInputBuffer();
+    while (Serial.available() == 0){}
+    float distance = Serial.parseFloat();
     s1.write(180);
+    //Serial.println(position);
     motorSpeed = 120;
     analogWrite(5, motorSpeed);
     analogWrite(6, motorSpeed);
+    while (position < distance)
+    {
+      sensors_event_t a, g, temp;
+      mpu.getEvent(&a, &g, &temp);
+      unsigned long now = micros();
+      if (counter == 0)
+      {
+        counter++;
+        lastTime = now;
+      }
+      float dt = (now - lastTime) / 1000000.0f;  // dt in seconds
+      lastTime = now;
+      Serial.println(dt);
+      float accelX_rate = sqrt(pow((a.acceleration.x - xoffset), 2) + pow((a.acceleration.y - yoffset), 2));
+      position += abs(accelX_rate * (dt*dt));
+      Serial.println(position);
+    }
+    analogWrite(5, 0);
+    analogWrite(6, 0);
     Serial.println("Done1");
   }
   else if (iput == 2)
   {
     clearInputBuffer();
+    while (Serial.available() == 0){}
+    float distance = Serial.parseFloat();
     s1.write(180);
     motorSpeed = 180;
     analogWrite(5, motorSpeed);
     analogWrite(6, motorSpeed);
+    while (position < distance)
+    {
+      sensors_event_t a, g, temp;
+      mpu.getEvent(&a, &g, &temp);
+      unsigned long now = micros();
+      if (counter == 0)
+      {
+        counter++;
+        lastTime = now;
+      }
+      float dt = (now - lastTime) / 1000000.0f;  // dt in seconds
+      lastTime = now;
+      Serial.println(dt);
+      float accelX_rate = sqrt(pow((a.acceleration.x - xoffset), 2) + pow((a.acceleration.y - yoffset), 2));
+      position += abs(accelX_rate * (dt*dt));
+      Serial.println(position);
+    }
+    analogWrite(5, 0);
+    analogWrite(6, 0);
     Serial.println("Done2");
   }
   else if (iput == 3)
   {
     clearInputBuffer();
+    while (Serial.available() == 0){}
+    float distance = Serial.parseFloat();
     s1.write(180);
     motorSpeed = 240;
     analogWrite(5, motorSpeed);
     analogWrite(6, motorSpeed);
+    while (position < distance)
+    {
+      sensors_event_t a, g, temp;
+      mpu.getEvent(&a, &g, &temp);
+      unsigned long now = micros();
+      if (counter == 0)
+      {
+        counter++;
+        lastTime = now;
+      }
+      float dt = (now - lastTime) / 1000000.0f;  // dt in seconds
+      lastTime = now;
+      Serial.println(dt);
+      float accelX_rate = sqrt(pow((a.acceleration.x - xoffset), 2) + pow((a.acceleration.y - yoffset), 2));
+      position += abs(accelX_rate * (dt*dt));
+      Serial.println(position);
+    }
+    analogWrite(5, 0);
+    analogWrite(6, 0);
     Serial.println("Done3");
   }
   else if (iput == 4)
@@ -88,8 +156,8 @@ void loop()
     Serial.println(turnAngleR);
     analogWrite(5, 180);
     analogWrite(6, 50);
-    s1.write(125);
-    while (angleZ < turnAngleR)
+    s1.write(115);
+    while (angleZ <= turnAngleR)
     {
       sensors_event_t a, g, temp;
       mpu.getEvent(&a, &g, &temp);
@@ -98,7 +166,6 @@ void loop()
       lastTime = now;
       float gyroZ_rate = ((g.gyro.z - zoffset) * (180/M_PI));  // deg/s
       angleZ += abs(gyroZ_rate * dt);
-      Serial.println(angleZ);
     }
     s1.write(180);
     analogWrite(5, 0);
@@ -113,7 +180,7 @@ void loop()
     Serial.println(turnAngleL);
     analogWrite(6, 180);
     analogWrite(5, 0);
-    s1.write(125);
+    s1.write(115);
     while (angleZ <= turnAngleL)
     {
       sensors_event_t a, g, temp;
@@ -144,3 +211,4 @@ void clearInputBuffer() {
     Serial.read(); 
   }
 }
+
